a67a468762461ce713ceca3a1911fd8c47277fad
Ronin <rlombardino44@gmail.com>
2021-12-22 17:04:36 -0800

    changed all api components to functions fixed formating in server/index

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const port = 3000
-const public = path.join(__dirname, '../client/dist')
-app.use(express.static(public))
-app.use(express.json())
+const app = express();
+const port = 3000;
+const publicPath = path.join(__dirname, '../client/dist');
+app.use(express.static(publicPath));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`listening at http://localhost:${port}`);
+});

87fe1c0db0d09c15120bd58cfdeba365995264df
Jesse Huang <jjhuang417@gmail.com>
2021-12-25 13:08:59 -0800

    Adjusted minor errors and implemented code clean-up.

--- client/src/components/related/Related.jsx
+++ client/src/components/related/Related.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import RelatedProductsList from './RelatedProductsList.jsx';
 import products from '../../sample-data/products.js';
 
-const Related = (props) => {
+const Related = () => {
   // state
   const [relatedProducts, setRelatedProducts] = useState(products);
 

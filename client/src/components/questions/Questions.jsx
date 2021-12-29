2f20cf91e6d4a4e63aaca775d73b5640c4213af7
Ronin <rlombardino44@gmail.com>
2021-12-29 05:05:49 -0800

    rendered basic QandA from hardcoded data

--- client/src/components/questions/Questions.jsx
+++ client/src/components/questions/Questions.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import QuestionRender from './QuestionRender.jsx'
+import data from '../../sample-data/questions.js'
 
-const Questions = () => (
-  <h1>Questions</h1>
-)
+const Questions = () => {
+  return (
+    <div>
+      <h1>
+        Questions
+      </h1>
+      <div>
+        {data.results.map((question) => <QuestionRender question={question}/>)}
+      </div>
+    </div>
+  )
+}
 
 export default Questions;
